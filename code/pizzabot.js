dd317cfe510a5095ddc4c118ddb6d3754c8b712f
Keb78 <114394959+Keb78@users.noreply.github.com>
2023-03-15 21:49:40 +0100

    Update
    Update

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -3,9 +3,7 @@ const hawaian = "Hawaian Pizza"
 const pepperoni = "Pepperoni Pizza"
 let orderName = ""
 let orderQuantity = 0
-let orderTotal = 0
-
-
+const message2 = `Get out of here!`
 const pizzaPrice = 80
 
 //Put your Javscript code here:
@@ -16,9 +14,39 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 
 orderName = prompt("Enter the name of the pizza you want to order today.");
-orderQuantity = prompt (`How many of ${orderName}  do you want.`);
 
 
-orderTotal = orderQuantity * pizzaPrice;
-const message = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`
-alert(message);
+if (checkOrderName(orderName)) {
+    orderQuantity = prompt(`How many of ${orderName}  do you want.`);
+    const message = `Great, I'll get started on your ${orderName} right away, it will take ${cookingTime(orderQuantity)} minutes and cost ${totalCost(orderQuantity)} kr`
+    alert(message)
+}
+
+else {
+    alert(message2);
+}
+
+function checkOrderName(orderName) {
+    if (orderName === vegetarian || orderName === hawaian || orderName === pepperoni) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function totalCost(orderQuantity) {
+        return orderQuantity * pizzaPrice;
+}
+
+function cookingTime(orderQuantity) {
+    if (orderQuantity <= 3) {
+        return 10
+    } else if (orderQuantity <= 5) {
+        return 15
+    } else if (orderQuantity >= 6) {
+        return 20
+    }
+
+
+}
